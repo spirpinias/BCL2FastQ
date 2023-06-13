8f3fc8e4fa510c866bb76c9ae637f891f3baba7f
Stephen Pirpinias <stephen@codeocean.com>
2023-06-13 00:31:23 +0000

    Added runBCL2.sh

---
+++ code/runBCL2.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+basecalls=$(find -L ../data -type d -name "BaseCalls")
+flowcell=$(find -L ../data -type d -name "flowcell")
+
+./bcl2fastq -i ${basecalls} -R /root/capsule/data/flowcell/ -o ${flowcell} --ignore-missing-bcls --ignore-missing-filter --ignore-missing-positions --ignore-missing-controls 
+
