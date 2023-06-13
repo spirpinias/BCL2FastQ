e344f0c5b22695cad7f5ddd168d76576c6e46e81
Stephen Pirpinias <stephen@codeocean.com>
2023-06-13 00:37:17 +0000

    Edited runBCL2.sh

--- code/runBCL2.sh
+++ code/runBCL2.sh
@@ -5,7 +5,7 @@ source ./config.sh
 basecalls=$(find -L ../data -type d -name "BaseCalls")
 flowcell=$(find -L ../data -type d -name "flowcell")
 
-./bcl2fastq \
+/root/capsule/code/usr/local/bin/./bcl2fastq \
 -i ${basecalls} \
 -R ${flowcell} \
 -o ../results/ \
